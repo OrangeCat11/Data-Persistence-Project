462fd0aa3a42548d2eee911b8cb62b17af4ba33f
OrangeCat11 <91157330+OrangeCat11@users.noreply.github.com>
2021-09-28 10:20:30 -0400

    commit 3
    Added functionality to the Start and Quit buttons.

---
+++ Assets/Scripts/MenuToStart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuToStart : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+}
